2356c1b3fac2623c53dd1216c62c220a39b5ca58
WillianDambros <126815136+WillianDambros@users.noreply.github.com>
2023-10-04 18:25:48 -0400

    Create sefaz_mt_receita_icms_cnae.R
    https://www5.sefaz.mt.gov.br/-/6847228-receita?ciclo=

---
+++ sefaz_mt_receita_icms_cnae.R
@@ -0,0 +1,73 @@
+# Downloading arquive ICMS_CNAE
+
+icms_cnae_endereco <- 
+  paste0("https://www5.sefaz.mt.gov.br/documents/6071037/51381700/",
+         "1-+ICMS+por+CNAE+e+Grupo+CNAE.xlsx/",
+         "0061779d-ce51-8852-fa19-94a757a5fad7?t=1695161941480")
+
+nome_destino <- 
+  paste0("Z:/rstudio/sefaz_mt/receita/sefaz_mt_receita_icms_cnae.R",
+         "icms_cnae", ".xlsx")
+
+curl::curl_download(icms_cnae_endereco, nome_destino)
+
+# Transforming Microdata
+
+icms_cnae_arquivo <-
+  "Z:/rstudio/sefaz_mt/receita/sefaz_mt_receita_icms_cnae.Ricms_cnae.xlsx"
+
+icms_cnae_folhas <- readxl::excel_sheets(icms_cnae_arquivo)
+
+icms_cnae_vetor <- vector(mode = 'list', length = (length(icms_cnae_folhas)))
+
+
+process_icms_cnae_data <- function(entrada) {
+  # read to define columns names
+  icms_cnae_names <-
+    readxl::read_excel(icms_cnae_arquivo, sheet = entrada, col_names = F,
+                       col_types = c("text", "text", "text", "text","text",
+                                     "text","date", "date", "date", "date",
+                                     "date","date", "date", "date", "date",
+                                     "date", "date", "date", "text"))
+  # extracting column names
+  icms_cnae_names <- icms_cnae_names[5,]
+  # creating list to store values of a vector
+  icms_cnae_names_vetor <- vector(length = ncol(icms_cnae_names))
+  # store properly the values
+  for(i in seq_along(icms_cnae_names)){
+    icms_cnae_names_vetor[i] <- as.character(icms_cnae_names[[i]])
+  }
+  # reading data
+  icms_cnae <- readxl::read_excel(icms_cnae_arquivo, sheet = entrada,
+                                  col_names = F, col_types = "text")
+  
+  icms_cnae <- icms_cnae |> dplyr::rename_with(~icms_cnae_names_vetor,
+                                               .cols = 1:ncol(icms_cnae))
+  icms_cnae <- icms_cnae |>
+    dplyr::filter(!stringr::str_detect(SEÇÃO, "SEÇÃO|TOTAL|FONTE|Obs|Soma")) |>
+    dplyr::select(!TOTAL) |>
+    tidyr::pivot_longer(matches("\\d{4}-\\d{2}-\\d{2}"), names_to = "data_mes")
+}
+
+
+for(i in seq_along(icms_cnae_folhas)){
+  
+  tryCatch({
+  icms_cnae_vetor[[i]] <- process_icms_cnae_data(icms_cnae_folhas[i])
+  }, error = function(err){warning("file not processed")})
+  
+}
+
+sefaz_icms_cnae <- icms_cnae_vetor |> dplyr::bind_rows()
+
+sefaz_icms_cnae <- sefaz_icms_cnae |>
+  dplyr::mutate(across(matches("value"), as.numeric))
+
+# Writing novocaged
+
+nome_arquivo_csv <- "sefaz_mt_receita_icms_cnae"
+
+caminho_arquivo <- paste0(getwd(), "/", nome_arquivo_csv, ".txt")
+
+readr::write_csv2(sefaz_icms_cnae,
+                  caminho_arquivo)
